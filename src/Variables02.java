7682e53e6aaa3581b116e5f870eef23653a3e3d0
Aluno <Aluno@LAB04-PC09>
2024-06-05 21:19:54 -0300

    Commit project Variable02

---
+++ src/Variables02.java
@@ -0,0 +1,19 @@
+public class Variables02 {
+
+    public static void main(String[] args) {
+        //Etapa1: 
+        String x = "puppy";
+        System.out.println(x);
+        System.out.println(x);
+        //Etapa2:
+        x = "kitty";
+        System.out.println(x);
+        System.out.println(x);
+        //Etapa3:
+        System.out.println(x);
+        x = "bunny";
+        System.out.println(x);
+        
+        
+    }
+}
